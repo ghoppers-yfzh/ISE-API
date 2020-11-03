0294432439a0f608426659b0a81183e3452b1658
wukong-sun <35114702+wukong-sun@users.noreply.github.com>
2020-11-03 13:22:40 +1300

    Update ISE-API.py
    Add function to select the def

--- ISE-API.py
+++ ISE-API.py
@@ -14,21 +14,21 @@ def endpoint_groups():
         uuid = i[1]
         print ("{0:22s} {1:36s}".format(groupname, uuid))
 
-#function for polling a network device detail
-def device_details():
-    print(ise.get_device(device='Test-9300-1')['response'])
+#function for polling a network device detail 'Test-9300-1'
+def device_details(network_device):
+    print(ise.get_device(device=network_device)['response'])
     
-#function for polling the endpoint group details
-def endpoint_group_details():
-    print(ise.get_endpoint_group(group='Cisco-IP-Phone')['response'])
+#function for polling the endpoint group details 'Cisco-IP-Phone'
+def endpoint_group_details(endpoint_group):
+    print(ise.get_endpoint_group(group=endpoint_group)['response'])
 
 #function for populate a list of endpoint in the database
 def endpoint_list():
     print(ise.get_endpoints['response'])
 
-#function for polling the endpoint details and print out the group name
-def endpoint_details():
-    device = ise.get_endpoint(mac_address='00:D6:FE:EC:12:A9')['response']
+#function for polling the endpoint details and print out the group name '00:D6:FE:EC:12:A9'
+def endpoint_details(mac):
+    device = ise.get_endpoint(mac_address= mac)['response']
     print(json.dumps(device, indent = 4))
     groupid = device['groupId']
     for i in ise.get_endpoint_groups()['response']:
@@ -37,5 +37,31 @@ def endpoint_details():
             print ("{0:22s} {1:36s}".format('Endpoint Group:', groupname))
         else:
             pass
+
+def func_select():
+    while(True):
+        print("1. List Endpoint Groups")
+        print("2. Endpoint Group Details")
+        print("3. Endpoint Deatils")
+        print("4. Network Device Details")
+        print("0. EXIT(0)")
+        select_input = input("SELECT => ")
+        if select_input == "0":
+            break
+        elif select_input == "1":
+            endpoint_groups()
+        elif select_input == "2":
+            input_val = input("Endpoint Group Name => ")
+            endpoint_group_details(input_val)
+        elif select_input == "3":
+            input_val = input("Endpoint MAC => ")
+            endpoint_details(input_val)
+        elif select_input == "4":
+            input_val = input("Network Device Name => ")
+            device_details(input_val)
+        else:
+            print("input error!!!")
+            continue
+
 if __name__ == "__main__":
-    endpoint_details()
+    func_select()
